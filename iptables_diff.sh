768a984c2d7ecf373df22216e93642cd842822b3
daneos <daneos@daneos.com>
2012-12-14 16:05:35 +0100

    not working :(

--- iptables_diff.sh
+++ iptables_diff.sh
@@ -3,7 +3,7 @@
 function usage() {
 	cat << EOF
 Usage: $0 <OPTIONS>
-Prints iptables command for change from chain FROM to TO
+Prints iptables commands f-o change from chain FROM to TO
 Options:
 -f FROM chain (file)
 -t TO chain (file)
@@ -36,17 +36,47 @@ while getopts "hf:t:" OPTION; do
 	esac
 done
 
-[ -n "$from" ] || { echo "No FROM chain specified" ; exit 1; }
-[ -n "$to" ] || { echo "No TO chain specified" ; exit 1; }
+[ -n "$from" ] || { echo "No FROM chain specified"; usage; exit 1; }
+[ -n "$to" ] || { echo "No TO chain specified"; usage; exit 1; }
 
-# --- not sure if working --- #
-diff --suppress-common-lines -d $from $to | while read line
+# --- not sure if troll or working --- #
+# diff --suppress-common-lines -d $from $to | while read line
+# do
+	# if [ "$(echo $line | cut -d' ' -f1)" == "<" ]; then
+		# if [ "$(echo $line | cut -d' ' -f2)" != "-P" ]; then
+			# echo $line | sed -e 's/-A /-D /' -e 's/[<>]/iptables/'
+		# fi
+	# elif [ "$(echo $line | cut -d' ' -f1)" == ">" ]; then
+		# echo $line | sed -e 's/[<>]/iptables/'
+	# fi
+# done
+
+numln=$(cat $from | wc -l)
+
+cat $to | while read line
 do
-	if [ "$(echo $line | cut -d' ' -f1)" == "<" ]; then
-		if [ "$(echo $line | cut -d' ' -f2)" != "-P" ]; then
-			echo $line | sed -e 's/-A /-D /' -e 's/[<>]/iptables/'
-		fi
-	elif [ "$(echo $line | cut -d' ' -f1)" == ">" ]; then
-		echo $line | sed -e 's/[<>]/iptables/'
+	# if [ $(echo $line | grep -F -e '-j\
+# --jump\
+# -g\
+# --goto') ]
+	# then
+		# echo "# --- jump or goto detected, make sure that target exists --- #"
+		# echo "iptables $line"
+		# echo "# ----------------------------------------------------------- #"
+	# fi
+	echo $(echo $line | grep -F -e '-j\
+--jump\
+-g\
+--goto')
+	echo "iptables $line"
+done
+
+i=1
+cat $from | while read line
+do
+	if [ "$i" -gt "$numln" ]; then
+		exit 0
 	fi
+	echo "iptables -D $(echo $line | cut -d' ' -f2) $i"
+	(( i++ ))
 done

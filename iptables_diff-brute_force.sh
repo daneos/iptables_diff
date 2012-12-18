2554b832fadda16bf886ecd097e4800a697bbb5b
daneos <daneos@daneos.com>
2012-12-18 11:36:47 +0100

    shortening script

--- iptables_diff-brute_force.sh
+++ iptables_diff-brute_force.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-function usage() {
-	cat << EOF
-Usage: $0 <OPTIONS>
-Prints iptables commands for change from chain FROM to TO
-Options:
--f FROM chain (file)
--t TO chain (file)
--d destination script (file)
--T generate test
--h Prints this help message
-Example: $0 -f ipt_from -t /root/ipt -d setipt.sh -T 
-EOF
+function usage()
+{
+	cat <<- EOF
+		Usage: $0 <OPTIONS>
+		Prints iptables commands for change from chain FROM to TO
+		Options:
+		-f FROM chain (file)
+		-t TO chain (file)
+		-d destination script (file)
+		-T generate test
+		-h Prints this help message
+		Example: $0 -f ipt_from -t /root/ipt -d setipt.sh -T 
+	EOF
 }
 
 while getopts "hTf:t:d:" OPTION; do
@@ -48,55 +49,40 @@ done
 [ -n "$to" ] || { echo "No TO chain specified"; usage; exit 1; }
 [ -n "$dest" ] || { echo "No destination specified"; usage; exit 1; }
 
-# --- not sure if troll or working --- #
-# diff --suppress-common-lines -d $from $to | while read line
-# do
-	# if [ "$(echo $line | cut -d' ' -f1)" == "<" ]; then
-		# if [ "$(echo $line | cut -d' ' -f2)" != "-P" ]; then
-			# echo $line | sed -e 's/-A /-D /' -e 's/[<>]/iptables/'
-		# fi
-	# elif [ "$(echo $line | cut -d' ' -f1)" == ">" ]; then
-		# echo $line | sed -e 's/[<>]/iptables/'
-	# fi
-# done
+function gentestcode()
+{
+	cat <<- EOF
+		set -e
+		chain=DIFFTMP\$RANDOM
+		trap 'iptables -X \$chain; echo \"An error occured, could not add all rules to test chain. Maybe some jump/goto targets do not exist?\"' EXIT
+		iptables -N \$chain
+	EOF
+	sed -e '/^-./ s/ [A-Za-z]* / \$chain /' -e '/^-./ s/^-/iptables -/' $to
+	cat <<- EOF
+		iptables -X \$chain
+		set +e
+		trap - EXIT
+	EOF
+}
 
-echo $(head -1 $0) > $dest
-destf=$(mktemp)
+function genscript()
+{
+	sed -e '/^-./ s/^-/iptables -/' $to
+	numln=$(wc -l < $from)
+	while read line
+	do
+		if [ "$numln" == "0" ]; then
+			exit 0
+		fi
+		chain=$(echo $line | cut -d' ' -f2)
+		echo "iptables -D $chain $numln"
+		(( numln-- ))
+	done < $from
+}
 
+head -1 $0 > $dest
 if [ "$gentest" ]; then
-	testf=$(mktemp)
-	echo "set -e" > $testf
-	echo "chain=DIFFTMP\$RANDOM" >> $testf
-	echo "trap 'iptables -X \$chain; echo -e \"\e[1;31mAn error occured, could not add all rules to test chain. Maybe some jump/goto targets do not exist?\e[00m\"' EXIT" >> $testf
-	echo "iptables -N \$chain" >> $testf
+	gentestcode >> $dest
 fi
-
-cat $to | while read line
-do
-	if [ "$gentest" ]; then
-		echo "iptables $(echo $line | sed -e '/^-./ s/ [A-Za-z]* / \$chain /')" >> $testf
-	fi
-	echo "iptables $line" >> $destf
-done
-
-numln=$(cat $from | wc -l)
-cat $from | while read line
-do
-	if [ "$numln" == "0" ]; then
-		exit 0
-	fi
-	echo "iptables -D $(echo $line | cut -d' ' -f2) $numln"  >> $destf
-	(( numln-- ))
-done
-
-if [ "$gentest" ]; then
-	echo "iptables -X \$chain" >> $testf
-	echo "set +e" >> $testf
-	echo "trap - EXIT" >> $testf
-	cat $testf >> $dest
-	rm $testf
-fi
-
-cat $destf >> $dest
-rm $destf
+genscript >> $dest
 chmod +x $dest
